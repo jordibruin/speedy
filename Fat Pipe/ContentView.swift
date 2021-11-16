9635fd12303eb364423b921c86bfdce1b405523b
Jordi Bruin <jordi@goodsnooze.com>
2021-11-16 22:32:58 +0100

    Initial Commit

---
+++ Fat Pipe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fat Pipe
+//
+//  Created by Jordi Bruin on 16/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fat Pipe/Fat_PipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fat_PipeApp.swift
+//  Fat Pipe
+//
+//  Created by Jordi Bruin on 16/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct Fat_PipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
